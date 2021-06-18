aeb5de5926efa3b01bf85dd8a4a9ee37aa825633
Your <tilley_l@hotmail.com>
2021-06-18 20:10:59 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+  return (
+    <div className="App">
+ <header>
+   <h1>LYLE'S TO-DO APP</h1>
+        
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
